f4f17b94b22733487f627117b2cc03e521e6f85e
Graeme Woods <graemewoods202@gmail.com>
2019-07-01 21:47:59 -0400

    remove bulma and reset

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-	@import url('https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css');
-	@import url('https://meyerweb.com/eric/tools/css/reset/reset.css');
-
 	body {
 		font-size: 16px;
 		padding: 2rem;
