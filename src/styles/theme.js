df5fa0ad812a50b206d280833da3938e22fd3c0f
Graeme Woods <graemewoods202@gmail.com>
2019-07-01 23:02:31 -0400

    theme colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,12 @@
 export default {
-	font: 'sans-serif',
-	colors: {
-		primary: 'tomato',
-		secondary: 'cornflowerblue',
+	fonts: {
+		title: 'sans-serif',
+		body: 'serif',
+	},
+	color: {
+		brightGreen: '#17B890',
+		lightGreen: '#32DE8A',
+		light: '#CBF3F0',
+		dark: '#5B5B5B',
 	},
 };
