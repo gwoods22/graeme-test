7374c909db65e5d3045b2b76b9e2804ca13f99ca
Graeme Woods <graemewoods202@gmail.com>
2019-07-04 18:16:13 -0400

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+const Styled404 = styled.div`
+	margin-top: 1rem;
+`;
+
 const NotFoundPage = () => (
 	<Layout>
 		<SEO title="404" />
-		<h1>NOT FOUND</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<Styled404>
+			<div className="center">
+				<h1>NOT FOUND</h1>
+				<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+			</div>
+		</Styled404>
 	</Layout>
 );
 
