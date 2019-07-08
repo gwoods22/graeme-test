75d8da3604d2c5106b5c80c2db95e31a772e6217
Graeme Woods <graemewoods202@gmail.com>
2019-07-08 16:40:34 -0400

    Revert "404"
    This reverts commit 7374c909db65e5d3045b2b76b9e2804ca13f99ca.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const Styled404 = styled.div`
-	margin-top: 1rem;
-`;
-
 const NotFoundPage = () => (
 	<Layout>
 		<SEO title="404" />
-		<Styled404>
-			<div className="center">
-				<h1>NOT FOUND</h1>
-				<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-			</div>
-		</Styled404>
+		<h1>NOT FOUND</h1>
+		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
 	</Layout>
 );
 
