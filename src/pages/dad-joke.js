100b2c018f8431e3c7581b41c05b89ed9315afb4
Graeme Woods <graemewoods202@gmail.com>
2019-07-25 13:26:16 -0400

    export function

--- src/pages/dad-joke.js
+++ src/pages/dad-joke.js
@@ -3,11 +3,9 @@ import SEO from '../components/seo';
 import Layout from '../components/layout';
 import DadJokeTemplate from '../templates/dad-joke-template';
 
-const PageTwo = () => (
+export default () => (
 	<Layout>
 		<SEO title="Dad Jokes" />
 		<DadJokeTemplate />
 	</Layout>
 );
-
-export default PageTwo;
