dd0e7aad447658af6bcf5a24359b32161eb1e031
Graeme Woods <graemewoods202@gmail.com>
2019-07-25 13:18:41 -0400

    validate functionality

--- src/templates/validate-template.js
+++ src/templates/validate-template.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { domain } from '../util/email';
 
 const StyledValidate = styled.div`
 	.validate {
 		padding: 4rem 6rem;
 	}
+
 	form {
 		display: flex;
 		flex-direction: column;
@@ -13,8 +15,9 @@ const StyledValidate = styled.div`
 		font-size: 1.3rem;
 		margin: 2rem 0 0.5rem;
 	}
-	label {
-		margin-bottom: 1rem;
+	span {
+		margin: 0.1rem 0 1rem;
+		font-size: 0.8rem;
 	}
 	textarea {
 		width: 300px;
@@ -24,31 +27,63 @@ const StyledValidate = styled.div`
 	.submit-container {
 		margin-top: 0.5rem;
 	}
+	th {
+		font-weight: 700;
+	}
 `;
 
-const ValidateTemplate = () => {
+export default () => {
 	const [input, setInput] = useState('');
 	const [results, setResults] = useState([]);
+	const [stats, setStats] = useState([]);
 	const [showResults, setShowResults] = useState(false);
 
-	const handleChange = (event) => {
-		setInput(event.target.value);
+	const badEmails = domain.split(',');
+
+	// Remove non domain characters, check if domain is a badEmail
+
+	const isReal = (email) => {
+		const searchDomain = email.split('@')[1].trim();
+		let i,
+			realVal = true;
+		for (i in badEmails) {
+			if (badEmails[i] === searchDomain) realVal = false;
+		}
+		return realVal;
 	};
 
 	const handleSubmit = (event) => {
 		let emails = input.split(',');
 		let i,
+			fakes = 0,
 			resultList = [];
+		// Remove whitespace and create resultList object array with isReal values
 		for (i in emails) {
-			// let data = { email: emails[i], real: disposable.validate(emails[i]) };
-			let data = { email: emails[i], real: true };
-			console.log(data);
-
-			resultList.push(data);
+			if (emails[i] !== ' ' && emails[i] !== '') {
+				let data = { email: emails[i], real: isReal(emails[i]) };
+				resultList.push(data);
+				if (!isReal(emails[i])) fakes++;
+			}
 		}
 		event.preventDefault();
 		setShowResults(true);
 		setResults(resultList);
+
+		// Stats data
+		const len = resultList.length;
+		setStats([
+			{ text: 'Total emails', value: len, className: '' },
+			{
+				text: 'Fake Email %',
+				value: `${((fakes / len) * 100).toFixed(1)}%`,
+			},
+			{ text: 'Fake Emails', value: fakes, className: 'has-text-danger' },
+			{
+				text: 'Real Emails',
+				value: len - fakes,
+				className: 'has-text-success',
+			},
+		]);
 	};
 
 	return (
@@ -60,40 +95,57 @@ const ValidateTemplate = () => {
 							<label className="has-text-weight-bold" htmlFor="emails">
 								Enter emails here:
 							</label>
+							<span>(comma separated)</span>
 							<textarea
 								className="textarea"
 								id="emails"
 								type="text"
-								onChange={handleChange}
+								onChange={(e) => setInput(e.target.value)}
 							/>
-
 							<div className="submit-container">
 								<input className="button" type="submit" value="Submit" />
 							</div>
 						</form>
 						{showResults && (
-							<div className="results">
-								<h2>Results</h2>
-								<table className="table is-bordered">
-									<tbody>
-										<tr>
-											<th>Email:</th>
-											<th>Is Real</th>
-										</tr>
-										{results.map((x, i) => (
-											<tr key={i}>
-												<td>{x.email}</td>
-												<td
-													className={
-														x.real ? 'has-text-success' : 'has-text-danger'
-													}
-												>
-													{x.real.toString()}
-												</td>
-											</tr>
-										))}
-									</tbody>
-								</table>
+							<div className="columns">
+								<div className="column is-half">
+									<div className="results">
+										<h2>Results</h2>
+										<table className="table is-bordered">
+											<tbody>
+												<tr>
+													<th>Email:</th>
+													<th>Is Real</th>
+												</tr>
+												{results.map((x, i) => (
+													<tr key={i}>
+														<td>{x.email}</td>
+														<td
+															className={
+																x.real ? 'has-text-success' : 'has-text-danger'
+															}
+														>
+															{x.real.toString()}
+														</td>
+													</tr>
+												))}
+											</tbody>
+										</table>
+									</div>
+								</div>
+								<div className="column is-half">
+									<h2>Stats</h2>
+									<table className="table is-bordered">
+										<tbody>
+											{stats.map((x, i) => (
+												<tr key={i} className={x.className}>
+													<td>{x.text}</td>
+													<td>{x.value}</td>
+												</tr>
+											))}
+										</tbody>
+									</table>
+								</div>
 							</div>
 						)}
 					</div>
@@ -102,5 +154,3 @@ const ValidateTemplate = () => {
 		</>
 	);
 };
-
-export default ValidateTemplate;
